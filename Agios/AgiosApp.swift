1bbe7efa51a27818af63b6f3bec50ca0cac95afb
dankamel <danielufkamel@gmail.com>
2024-04-17 14:44:34 +0100

    Initial Commit

---
+++ Agios/AgiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgiosApp.swift
+//  Agios
+//
+//  Created by Daniel Kamel on 17/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AgiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
