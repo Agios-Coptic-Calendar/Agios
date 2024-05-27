9d2e91544844ceeea306434e3da5f548d337c107
zuriks <victorander18@gmail.com>
2024-05-27 16:42:11 +0100

    [Feature]: Worked on a launch view.

--- Agios/AgiosApp.swift
+++ Agios/AgiosApp.swift
@@ -14,14 +14,28 @@ struct AgiosApp: App {
     @StateObject private var imageViewModel = IconImageViewModel(icon: IconModel(id: "", created: "", updated: "", caption: "", image: "", croppedImage: "", iconagrapher: Iconagrapher(created: "", id: "", name: "", updated: "")))
     
     @Namespace var namespace
+    @State private var showLaunchView: Bool = true
     
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                HomeView(iconographer: dev.iconagrapher, namespace: namespace)
-                    .environmentObject(occasionViewModel)
-                    .environmentObject(imageViewModel)       
-            }
+            ZStack(content: {
+                NavigationStack {
+                    HomeView(iconographer: dev.iconagrapher, namespace: namespace)
+                        .environmentObject(occasionViewModel)
+                        .environmentObject(imageViewModel)
+                }
+                
+                ZStack(content: {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                            .transition(.opacity.animation(.easeOut(duration: 0.2)))
+                    }
+                })
+                .zIndex(2.0)
+                
+                
+            })
+            
             
         }
     }
--- Agios/Core/Launch/LaunchView.swift
+++ Agios/Core/Launch/LaunchView.swift
@@ -7,12 +7,70 @@
 
 import SwiftUI
 
+
 struct LaunchView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    @State private var logoText: [String] = "Agios".map {String($0)}
+    @State private var showLogoText: Bool = false
+    
+    private let timer = Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()
+    @State private var counter: Int = 0
+    @State private var loops: Int = 0
+    @Binding var showLaunchView: Bool
+    
+    var body: some View { 
+        ZStack(content: {
+            Rectangle()
+                .fill(.linearGradient(colors: [
+                    Color(red: 0.99, green: 0.98, blue: 0.96),
+                    Color(red: 0.96, green: 0.93, blue: 0.88),
+                    ],
+                                      startPoint: .top,
+                                      endPoint: .bottom))
+                .ignoresSafeArea()
+              
+            ZStack(content: {
+                if showLogoText {
+                    HStack(spacing: 0) {
+                        ForEach(logoText, id: \.self) { index in
+                            Text(index)
+                                .font(.largeTitle)
+                                .kerning(-1)
+                                .fontDesign(.rounded)
+                                .fontWeight(.bold)
+                                .foregroundStyle(.gray900)
+                        }
+                    }
+                    .transition(AnyTransition.push(from: .leading).animation(.easeIn))
+                }
+            })
+            .offset(x: showLogoText ? 17 : 0)
+            .animation(.spring(response: 0.35, dampingFraction: 1, blendDuration: 1), value: showLogoText)
+
+            
+            Image("appIcon")
+                .resizable()
+                .frame(width: showLogoText ? 65 : 170, height: showLogoText ? 65 : 170, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .offset(x: showLogoText ? -67 : 0) 
+                //.scaleEffect(showLogoText ? 0.42 : 1)
+                .animation(.spring(response: 0.35, dampingFraction: 1, blendDuration: 1), value: showLogoText)
+            
+        })
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
+                showLogoText.toggle()
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.8) {
+                showLaunchView = false
+            }
+            
+           
+        }
     }
 }
 
 #Preview {
-    LaunchView()
+    LaunchView(showLaunchView: .constant(true))
 }
+
+
