709c59f94f96262f064c6d110d00bf5965e823e5
Victor Onwuzuruike <victorander18@gmail.com>
2025-07-04 17:35:30 +0100

    Added a new empty search view

--- Agios/Core/Home/Views/Date/Feast/EmptySearchView.swift
+++ Agios/Core/Home/Views/Date/Feast/EmptySearchView.swift
@@ -1,3 +1,13 @@
+//
+//  EmptySearchView.swift
+//  Agios
+//
+//  Created by Victor Onwuzuruike on 04/07/2025.
+//
+
+import SwiftUI
+import Lottie
+
 struct EmptySearchView: View {
     var body: some View {
         VStack(spacing: -8) {
@@ -15,4 +25,8 @@ struct EmptySearchView: View {
         .frame(maxWidth: .infinity)
         .padding(.top, 40)
     }
+}
+
+#Preview {
+    EmptySearchView()
 }
